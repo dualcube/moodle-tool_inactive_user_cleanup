a0a204ffe4aa8475d3d81ba52f6b9fa2bf5ad973
parthajeet@dualcube.com <parthajeet@dualcube.com>
2015-09-11 12:33:56 +0530

    1st upload

---
+++ lib.php
@@ -0,0 +1,59 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Inactive user cleanup library
+ *
+ * @package    tool
+ * @subpackage inactive user cleanup
+ * @copyright  2014 dualcube {@link http://dualcube.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+ defined('MOODLE_INTERNAL') || die;
+/*
+ * Standard cron function
+ */
+require_once($CFG->libdir.'/adminlib.php');
+require_capability('moodle/user:delete', context_system::instance());
+function tool_inactive_user_cleanup_cron() {
+    global $DB, $CFG;
+    mtrace("Hey, admin tool inactive user cleanup is running");
+    $beforedelete = get_config('tool_inactive_user_cleanup', 'daysbeforedeletion');
+    $inactivity = get_config('tool_inactive_user_cleanup', 'daysofinactivity');
+    $subject = get_config('tool_inactive_user_cleanup', 'emailsubject');
+    $body = get_config('tool_inactive_user_cleanup', 'emailbody');
+    $users = $DB->get_records('user', array('deleted' => '0'));
+    $messagetext = html_to_text($body);
+    foreach ($users as $usersdetails) {
+        if (date("d-m-y", $usersdetails->lastlogin) - date("d-m-y") > $inactivity) {
+            if ($mailresults = email_to_user($usersdetails, $users, $subject, $messagetext)) {
+                mtrace('id');
+                mtrace($usersdetails->id);
+                mtrace('inactivity');
+                mtrace(date("d-m-y", $usersdetails->lastlogin) - date("d-m-y"));
+                mtrace('email sent');
+            }
+        }
+        if ($beforedelete != 0) {
+            if ((date("d-m-y", $usersdetails->lastlogin) - date("d-m-y")) >= ($inactivity + $beforedelete)) {
+                delete_user($usersdetails);
+                mtrace('delete user' . $usersdetails->id);
+            }
+        }
+    }
+    return true;
+}
+
---
+++ settings.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Link to inactive user cleanup
+ *
+ * @package    tool
+ * @subpackage inactive user cleanup
+ * @copyright  2014 dualcube {@link http://dualcube.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+if ($hassiteconfig) {
+    $ADMIN->add('reports',
+        new admin_externalpage('toolinactive_user_cleanup', get_string('pluginname', 'tool_inactive_user_cleanup'),
+        "$CFG->wwwroot/$CFG->admin/tool/inactive_user_cleanup/index.php", 'moodle/site:config'));
+}
+
+
+
