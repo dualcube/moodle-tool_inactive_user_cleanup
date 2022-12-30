0fb4b0c0e4dc27781a9979417b90b0435f1af207
kundandualcube <kundan@dualcube.com>
2022-12-30 20:44:40 +0530

    Revert "."
    This reverts commit e418a7553042a6d08ce854a655bc4d8dee6415c5.

--- classes/task/tool_inactive_user_cleanup_task.php
+++ classes/task/tool_inactive_user_cleanup_task.php
@@ -64,7 +64,7 @@ class tool_inactive_user_cleanup_task extends \core\task\scheduled_task {
                     if (!$ischeck &&  $usersdetails->lastaccess != 0) {
                         $record = new \stdClass();
                         $record->userid = $usersdetails->id;
-                        if (email_to_user($usersdetails, $mainadminuser, $subject, $messagetext)) {
+                        // if (email_to_user($usersdetails, $mainadminuser, $subject, $messagetext)) {
                             mtrace(get_string('userid','tool_inactive_user_cleanup'));
                             mtrace($usersdetails->id. '---' .$usersdetails->email);
                             mtrace(get_string('userinactivtime','tool_inactive_user_cleanup') . $minus);
@@ -72,7 +72,7 @@ class tool_inactive_user_cleanup_task extends \core\task\scheduled_task {
                             $record->emailsent = 1;
                             $record->date = time();
                             $lastinsertid = $DB->insert_record('tool_inactive_user_cleanup', $record, false);
-                        }
+                        // }
                     }
                 }
                 if ($beforedelete != 0 &&  $usersdetails->lastaccess != 0) {
